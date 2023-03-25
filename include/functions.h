7b6c2d9bd382936ff1bed0afd3b949fc5286fd54
Dimitrios Charalampidis <d.s.charalampidis@gmail.com>
2023-03-25 05:04:00 +0100

    Categorize functions

--- include/functions.h
+++ include/functions.h
@@ -13,13 +13,23 @@
 #ifndef FUNCTIONS_H
 # define FUNCTIONS_H
 
-char				*newline_strip(char *line);
+/* error_handling.c*/
 void				raise_error(char *err);
 bool				user_input(int argc, char **argv);
-void				free_split(char **tokens);
+
+/* create_data.c */
+void				init_data(struct s_data *data, char *file_map);
+bool				create_data(struct s_data *data, char *file_map);
+void				init_data(struct s_data *data, char *file_map);
+
+/* handle_lines.c */
+char				*strip_newline(char *line);
 bool				line_start_valid(char *str);
 bool				line_map(struct s_data *data, char *str);
 bool				line_is_empty(struct s_data *data, char *str);
+
+/* tmp.c */
+void				free_split(char **tokens);
 bool				texture_valid(char *str);
 bool				ft_str_is_numeric(char *str);
 bool				color_valid(char *color);
@@ -27,10 +37,12 @@ unsigned char		*scrap_colors(char *color);
 int					cnt_words(char *str, char delim);
 int					cub_strcmp(char *s1, char *s2);
 void				data_line_save(struct s_data *data, char *str);
-void				ft_lstadd_back_ms(struct s_map_list **lst, struct s_map_list *new);
+void				cub_lst_add_back(struct s_map_list **lst,
+						struct s_map_list *new);
 struct s_map_list	*create_node(char *str);
 struct s_map_list	*ft_lstlast(struct s_map_list *lst);
-void				map_list_save(struct s_map_list **list, struct s_map_list *node);
+void				map_list_save(struct s_map_list **list,
+						struct s_map_list *node);
 void				update_w_h(struct s_data *data, char *line);
 char				**create_map_array(struct s_data *data);
 bool				first_line_checker(char *line);
@@ -39,8 +51,6 @@ bool				line_checker(char *prev_line, char *line, char *next_line);
 bool				map_check(char **map, int height);
 int					**map_to_int_array(char **map, int height, int width);
 void				handle_maps(struct s_data *data);
-void				init_data(struct s_data *data, char *file_map);
-bool				data_create(struct s_data *data, char *file_map);
 void				draw(void);
 void				data_destroy(struct s_data *data);
 
---
+++ src/handle_lines.c
@@ -0,0 +1,81 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   handle_lines.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dcharala <dcharala@student.42heilbronn.de  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/25 04:50:17 by dcharala          #+#    #+#             */
+/*   Updated: 2023/03/25 04:54:35 by dcharala         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "cub3d.h"
+
+char*
+	strip_newline(char *line)
+{
+	int	len;
+
+	len = ft_strlen(line);
+	if (len > 0 && line[len - 1] == '\n')
+		line[len - 1] = '\0';
+	return (line);
+}
+
+bool
+	line_start_valid(char *str)
+{
+	if (str[0] == 'N'
+		|| str[0] == 'S'
+		|| str[0] == 'W'
+		|| str[0] == 'E'
+		|| str[0] == 'F'
+		|| str[0] == 'C')
+		return (true);
+	return (false);
+}
+
+void
+	data_line_save(struct s_data *data, char *str)
+{
+	char	**tokens;
+
+	tokens = ft_split(str, ' ');
+	if (cnt_words(str, ' ') > 2)
+		raise_error(TOO_MANY_ARGS_FOR_PATH);
+	if (!cub_strcmp(tokens[0], "NO") && texture_valid(tokens[1]))
+		data->no_fd = open(tokens[1], O_RDONLY);
+	else if (!cub_strcmp(tokens[0], "SO") && texture_valid(tokens[1]))
+		data->so_fd = open(tokens[1], O_RDONLY);
+	else if (!cub_strcmp(tokens[0], "WE") && texture_valid(tokens[1]))
+		data->we_fd = open(tokens[1], O_RDONLY);
+	else if (!cub_strcmp(tokens[0], "EA") && texture_valid(tokens[1]))
+		data->ea_fd = open(tokens[1], O_RDONLY);
+	else if (!cub_strcmp(tokens[0], "F") && color_valid(tokens[1]))
+		data->f_colors = scrap_colors(tokens[1]);
+	else if (!cub_strcmp(tokens[0], "C") && color_valid(tokens[1]))
+		data->c_colors = scrap_colors(tokens[1]);
+	else
+		raise_error(COLOR_INVALID);
+	free_split(tokens);
+}
+
+bool
+	line_map(struct s_data *data, char *str)
+{
+	if (str[0] == '1' || str[0] == ' ')
+	{
+		data->map_found = true;
+		return (true);
+	}
+	return (false);
+}
+
+bool
+	line_is_empty(struct s_data *data, char *str)
+{
+	if (data->map_found == true && str[0] == '\n')
+		return (true);
+	return (false);
+}
